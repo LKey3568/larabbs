33b749a0b0dc451c0d8723968145801cc3986b5b
vagrant <vagrant@homestead>
2019-06-18 09:04:35 +0000

    wysiwyg编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+    @yield('styles')
+
 </head>
 
 <body>
@@ -30,4 +32,6 @@
 
     <!-- scripts -->
     <script src="{{ mix('js/app.js') }}"></script>
+
+    @yield('scripts')
 </body>
