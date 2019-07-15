7d72a10f5a254add5b5884e796598acb761a3045
lkey_dell_deepin <lkey_email@163.com>
2019-07-15 10:16:05 +0800

    clone ceshsi
    clone

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use App\Handlers\ImageUploadHandler;
@@ -22,22 +21,28 @@ class UsersController extends Controller
     public function edit(User $user)
     {
         $this->authorize('update', $user);
+
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user )
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
         $this->authorize('update', $user);
         $data = $request->all();
 
-        if($request->avatar){
+        if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
-            if($result){
+            if ($result) {
                 $data['avatar'] = $result['path'];
             }
         }
 
         $user->update($data);
+
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功!');
+    }
+
+    public function clone()
+    {
     }
 }
